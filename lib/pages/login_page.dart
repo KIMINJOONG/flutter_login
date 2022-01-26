773d609175df00949b131fae923ea63e7f15d0e8
KIMINJOONG <ijboym216@gmail.com>
2022-01-26 22:07:31 +0900

    text form field design

--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_login/components/Logo.dart';
+import 'package:flutter_login/components/custom_text_form_field.dart';
 
 class LoginPage extends StatelessWidget {
 
@@ -7,10 +8,14 @@ class LoginPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: ListView(
-        children: [
-          Logo("Login"),
-        ],
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: ListView(
+          children: [
+            Logo("Login"),
+            CustomTextFormField(),
+          ],
+        ),
       ),
     );
   }
