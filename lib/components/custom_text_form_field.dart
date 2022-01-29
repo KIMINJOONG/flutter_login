307991f2e5d8226f910eb0bce363cd03223c9b96
KIMINJOONG <ijboym216@gmail.com>
2022-01-29 14:24:55 +0900

    text button theme

--- lib/components/custom_text_form_field.dart
+++ lib/components/custom_text_form_field.dart
@@ -1,15 +1,23 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_login/size.dart';
 
 class CustomTextFormField extends StatelessWidget {
+  final String text;
+
+  const CustomTextFormField({required this.text});
 
   @override
   Widget build(BuildContext context) {
+
     return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
-        Text("email"),
+        Text("$text"),
+        SizedBox(height: small_get),
         TextFormField(
+          obscureText: text == "password",
           decoration: InputDecoration(
-            hintText: "Enter Email",
+            hintText: "Enter $text",
             enabledBorder: OutlineInputBorder(
               borderRadius: BorderRadius.circular(20),
             ),
--- lib/pages/login_page.dart
+++ lib/pages/login_page.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_login/components/Logo.dart';
 import 'package:flutter_login/components/custom_text_form_field.dart';
+import 'package:flutter_login/size.dart';
 
 class LoginPage extends StatelessWidget {
 
@@ -12,8 +13,14 @@ class LoginPage extends StatelessWidget {
         padding: const EdgeInsets.all(16.0),
         child: ListView(
           children: [
+            SizedBox(height: xlarge_get,),
             Logo("Login"),
-            CustomTextFormField(),
+            SizedBox(height: large_get,),
+            CustomTextFormField(text: "email",),
+            SizedBox(height: medium_get,),
+            CustomTextFormField(text: "password",),
+            SizedBox(height: large_get,),
+            TextButton(onPressed: () {}, child: Text("Login"))
           ],
         ),
       ),
