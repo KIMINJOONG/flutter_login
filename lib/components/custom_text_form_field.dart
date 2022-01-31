e378f66d9b8a4e7cee1104eb5b994ac582622ea6
KIMINJOONG <ijboym216@gmail.com>
2022-01-31 13:09:57 +0900

    text form field widget error border

--- lib/components/custom_text_form_field.dart
+++ lib/components/custom_text_form_field.dart
@@ -31,6 +31,12 @@ class CustomTextFormField extends StatelessWidget {
             focusedBorder: OutlineInputBorder(
               borderRadius: BorderRadius.circular(20),
             ),
+            errorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
+            focusedErrorBorder: OutlineInputBorder(
+              borderRadius: BorderRadius.circular(20),
+            ),
           ),
         ),
       ],
